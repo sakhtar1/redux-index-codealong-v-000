2a320e5369bd53e2731273655cc41e5276044461
Sharmina Akhtar <sakht318@gmail.com>
2018-10-25 06:16:00 +0000

    automatically backed up by learn

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-
+import Todo from './Todo'
+ 
 class TodosContainer extends Component {
-
+ 
+  renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
+ 
   render() {
     return(
-      <div></div>
+      <div>
+        {this.renderTodos()}
+      </div>
     );
   }
 };
-
-export default connect()(TodosContainer);
+ 
+const mapStateToProps = state => {
+  return {
+    todos: state.todos
+  }
+}
+ 
+export default connect(mapStateToProps)(TodosContainer);
